175d2498dd39cfdfc347a0b596d24982b8820010
Ayush Sharma <aayusharma0220@gmail.com>
2024-02-23 02:31:32 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AppR from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <AppR />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
