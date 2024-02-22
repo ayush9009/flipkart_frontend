fa6b507ae2725debc4baec6e9e449f40248db145
Ayush Sharma <aayusharma0220@gmail.com>
2024-02-23 02:03:09 +0530

    fix

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+      </header>
+      <main>
+        <p>This is the main content of my application.</p>
+      </main>
+      <footer>
+        <p>&copy; 2022 My React App</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
