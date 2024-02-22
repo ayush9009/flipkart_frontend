c00d2869dc6e2a6aade6f0dbad61cb9b5c74c83d
Ayush Sharma <aayusharma0220@gmail.com>
2024-02-23 02:45:48 +0530

    fix:style

--- src/check.js
+++ src/check.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import AppRouter from './components/App.jsx';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
