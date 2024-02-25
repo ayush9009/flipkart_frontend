1d9d73b48e69a9743ded88e04b4e7ac5da136e55
aayush0001 <aayushbhardwaj708@gmail.com>
2024-02-25 23:25:23 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-
-    import React, { useState } from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import MenuIcon from "@mui/icons-material/Menu";
 // import "./logo.svg";
 // import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
+import { Button } from "react-bootstrap";
 
 const Header = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -15,55 +17,74 @@ const Header = () => {
 
   return (
     <div className="header">
-      <div className="first">
-        <img
-          src="//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_4ee2f9.png"
-          alt="Flipkart"
-        />
-        <div className="first1">
-          <span className="explore">Explore</span>
-          <span className="plus">Plus</span>
-          <span>
-            <img
-              width="10"
-              className="logo"
-              src="//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/plus_b13a8b.png"
-              alt=""
-            />
-          </span>
+      <div className="header-container">
+        <div className="menu">
+          <MenuIcon style={{ fontSize: "28px", color: "white" }} />
         </div>
-      </div>
-      <div className="second">
-        <input type="text" placeholder="Search for products, brands and more" />
-        <SearchIcon />
-      </div>
-      <div className="third">
-        <button onClick={handleLoginClick}>Login</button>
-        {showLogin && (
-          <div className="login__in">
-            {/* Your login content goes here */}
+        <div className="first">
+          <img
+            src="//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_4ee2f9.png"
+            alt="Flipkart"
+          />
+          <div className="first1">
+            <span className="explore">Explore</span>
+            <span className="plus">Plus</span>
+            <span>
+              <img
+                width="20"
+                className="logo"
+                src="//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/plus_b13a8b.png"
+                alt=""
+              />
+            </span>
           </div>
-        )}
-      </div>
-      {/* <div className="fourth">
+        </div>
+        <div className="second">
+          <input
+            type="text"
+            placeholder="Search for products, brands and more"
+          />
+          <SearchIcon />
+        </div>
+
+        {/* <div className="fourth">
         <p className="tp">More</p>
         <ExpandMoreIcon />
       </div> */}
-      <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic" style={{ backgroundColor: '#efefef', color: 'blue' }}>
-    More
-  </Dropdown.Toggle>
+        <div className="dropdown">
+          <Dropdown>
+            <Dropdown.Toggle
+              id="dropdown-basic"
+              size="md"
+              style={{
+                backgroundColor: "#efefef",
+                color: "blue",
+                border: "none",
+                padding: "7px 30px",
+                borderRadius: "0px",
+              }}
+            >
+              More
+            </Dropdown.Toggle>
 
-  <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-  </Dropdown.Menu>
-</Dropdown>
-
-      <div className="fifth">
-        {/* <img src={Logo} alt="" /> */}
-        <p>Cart</p>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className="third">
+          <Button variant="light" size="md">
+            Log In
+          </Button>
+        </div>
+        <div className="fifth-nav">
+          <div>
+            <AddShoppingCartIcon style={{ fontSize: "28px", color: "white" }} />
+          </div>
+          <h1>Cart</h1>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import img1 from '../assets/1.png';
-import './Navbar.css'; // Import your CSS file if you have external styles
+import React from "react";
+import img1 from "../assets/1.png";
+import "./Navbar.css"; // Import your CSS file if you have external styles
 
 const Navbar = () => {
   const myStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-   };
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
 
   return (
-    <div>
-      <div className="header">
-
-      </div>
+    <div className="outer-container">
       <div className="tab-container">
-        <section className="nav">
+        <section className="nav ">
           <img src={img1} />
           <p>Grocery</p>
         </section>
@@ -23,70 +20,92 @@ const Navbar = () => {
           <img src={img1} />
           <p>Mobile</p>
         </section>
-        <section class="nav dropdown">
-            <img src={img1}/>
-            <p style={myStyle}>Fashion
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
-                    <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
+        <section className="nav dropdown nav not-display" id="displaynot" >
+          <img src={img1} />
+          <p style={myStyle}>
+            Fashion
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-caret-down"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" />
+            </svg>
+          </p>
+          <div className="menu">
+            <ul className="main-menu">
+              <li>
+                Men's Top Wear{" "}
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-caret-down"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" />
                   </svg>
-            </p>
-            <div class="menu">
-              <ul class="main-menu">
-                <li>Men's Top Wear <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
-                    <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
-                  </svg></span>
-                    <ul class="inner-menu">
-                        <li>All</li>
-                        <li>Shirt</li>
-                        <li>T-Shirt</li>
-                      </ul>
-                </li>
-                <li>Men's Bottom Wear <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
-                    <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
-                  </svg></span>
-                    <ul class="inner-menu">
-                        <li>Shoe</li>
-                        <li>Slipper</li>
-                        <li>Special</li>
-                        <li>Crocs</li>
-                    </ul>
-                </li>
-                <li>Women Ethinc </li>
-                <li>Women Western </li>
-                <li>Women Footwear</li>
-                <li>Men Footwear </li>
-                <li>Kid </li>
-                <li>Winter </li>
-                <li>Summer </li>
-              </ul>
-
-            </div>
+                </span>
+                <ul className="inner-menu">
+                  <li>All</li>
+                  <li>Shirt</li>
+                  <li>T-Shirt</li>
+                </ul>
+              </li>
+              <li>
+                Men's Bottom Wear{" "}
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-caret-down"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" />
+                  </svg>
+                </span>
+                <ul className="inner-menu">
+                  <li>Shoe</li>
+                  <li>Slipper</li>
+                  <li>Special</li>
+                  <li>Crocs</li>
+                </ul>
+              </li>
+              <li>Women Ethinc </li>
+              <li>Women Western </li>
+              <li>Women Footwear</li>
+              <li>Men Footwear </li>
+              <li>Kid </li>
+              <li>Winter </li>
+              <li>Summer </li>
+            </ul>
+          </div>
         </section>
-        <section className="nav">
+        <section className="nav ">
           <img src={img1} />
           <p>Electronics</p>
         </section>
-        <section className="nav">
+        <section id="displaynot" className="nav not-display">
           <img src={img1} />
           <p>Home</p>
         </section>
-        <section className="nav">
+        <section id="displaynot" className="nav not-display">
           <img src={img1} />
           <p>Appliances</p>
         </section>
-        <section className="nav">
+        <section id="displaynot" className="nav not-display">
           <img src={img1} />
           <p>Travel</p>
         </section>
-        <section className="nav">
-          <img src={img1} />
-          <p>Top Offers</p>
-        </section>
-        <section className="nav">
-          <img src={img1} />
-          <p>Beauty, Toys & More</p>
-        </section>
-        <section className="nav">
+        
+        <section id="displaynot" className="nav displaynot">
           <img src={img1} />
           <p>Two Wheelers</p>
         </section>
@@ -96,3 +115,33 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {/* <section id="displaynot" className="nav displaynot">
+//           <img src={img1} />
+//           <p>Top Offers</p>
+//         </section>
+//         <section id="displaynot" className="nav displaynot">
+//           <img src={img1} />
+//           <p>Toys </p>
+//         </section> */}
