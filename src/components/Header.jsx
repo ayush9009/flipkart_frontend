a8504dead628f7f4e18fe61ca5268f4419760bb2
aayush0001 <aayushbhardwaj708@gmail.com>
2024-02-26 10:34:05 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import MenuIcon from "@mui/icons-material/Menu";
-// import "./logo.svg";
-// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Dropdown from "react-bootstrap/Dropdown";
 import { Button } from "react-bootstrap";
 
 const Header = () => {
-  const [showLogin, setShowLogin] = useState(false);
-
-  const handleLoginClick = () => {
-    setShowLogin(!showLogin);
-  };
-
+  
+  
   return (
     <div className="header">
       <div className="header-container">
-        <div className="menu">
-          <MenuIcon style={{ fontSize: "28px", color: "white" }} />
-        </div>
+        
         <div className="first">
           <img
             src="//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_4ee2f9.png"
@@ -46,12 +37,7 @@ const Header = () => {
           />
           <SearchIcon />
         </div>
-
-        {/* <div className="fourth">
-        <p className="tp">More</p>
-        <ExpandMoreIcon />
-      </div> */}
-        <div className="dropdown">
+        <div className="dropdown" >
           <Dropdown>
             <Dropdown.Toggle
               id="dropdown-basic"
@@ -66,7 +52,6 @@ const Header = () => {
             >
               More
             </Dropdown.Toggle>
-
             <Dropdown.Menu>
               <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
               <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
